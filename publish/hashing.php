e7d65ac93d940acaac00ce7eb5c0c74ddebf8870
hzmain <hzmain5@gmail.com>
2024-09-12 19:50:04 +0800

    update

--- publish/hashing.php
+++ publish/hashing.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
  * @contact  eric@zhu.email
  * @license  https://github.com/hyperf-ext/hashing/blob/master/LICENSE
  */
+use function Hyperf\Support\env;
 return [
     /*
     |--------------------------------------------------------------------------
